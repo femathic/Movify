29f27f520a289acd103c61d9a34886c61c0c8a4e
Femi Opanuga <femathic@gmail.com>
2020-03-06 14:45:08 +0100

    Adjust app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const store = configureStore();
+  const { getByText } = render(<Provider store={store}><App /></Provider>);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
